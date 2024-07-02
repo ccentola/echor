431c46643bf600f64185119de988fcf042d8b902
Carl Centola <carl.centola@gmail.com>
2024-07-02 13:37:05 -0700

    add testing

--- src/main.rs
+++ src/main.rs
@@ -20,10 +20,10 @@ fn main() {
         )
         .get_matches();
 
-    println!("{:#?}", matches);
+    let text: Vec<String> =
+        matches.get_many("text").unwrap().cloned().collect();
 
-    let text: Vec<String> = matches.get_many("text").unwrap().cloned().collect();
     let omit_newline = matches.get_flag("omit_newline");
 
-    print!("{}{}", text.join(" "), if omit_newline { "" } else { "\n"});
+    print!("{}{}", text.join(" "), if omit_newline { "" } else { "\n" });
 }
